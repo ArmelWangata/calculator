3cdbec39e0be43e9e8c767f894753ebdbe57a4d6
Armel Wangata <armelwangata23@gmail.com>
2025-11-25 11:29:53 -0500

    a new commit

--- script.js
+++ script.js
@@ -12,19 +12,6 @@ const deleteLast = () => {
   display.value = display.value.slice(0, -1);
 };
 
-const appendBrackets = () => {
-  const start = display.selectionStart;
-  const end = display.selectionEnd;
-
-  const text = display.value;
-
-  display.value = text.slice(0, start) + "()" + text.slice(end);
-
-  display.setSelectionRange(start + 1, start + 1);
-
-  display.focus();
-};
-
 const calculateResult = () => {
   try {
     display.value = eval(display.value);
